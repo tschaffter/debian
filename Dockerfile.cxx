b341276a9a7ea988a08c6261560d8e9ba86f3e04
tschaffter <thomas.schaffter@gmail.com>
2020-07-19 07:05:39 -0700

    Separate install and version print into two RUN

--- Dockerfile.cxx
+++ Dockerfile.cxx
@@ -1,20 +1,36 @@
 ARG version
-FROM tschaffter/debian:user-${version}
+FROM tschaffter/debian:python-${version}
 
-
+# Install required tools
 RUN sudo apt-get update -qq -y \
     && sudo apt-get install --no-install-recommends -qq -y \
-        gnupg2 \
-        software-properties-common
+        gcc \
+        g++ \
+        ninja-build \
+        git \
+        lcov \
+        curl \
+        sudo \
+        wget \
+        clang-format-9 \
+    && ln -s /usr/bin/clang-format-9 /usr/bin/clang-format \
+    && mv /usr/bin/lsb_release /usr/bin/lsb_release.bak \
+    && apt-get -y autoclean \
+    && apt-get -y autoremove \
+    && rm -rf /var/lib/apt-get/lists/*
 
-# Install Python (cmake-format)
-RUN sudo add-apt-repository ppa:deadsnakes/ppa
-    # sudo apt-get update -qq -y \
-    # && sudo apt-get install --no-install-recommends -qq -y \
-    #     python3.9 \
-    #     python3-pip \
-    # && sudo ln -s -f "$(command -v python3.9)" /usr/bin/python \
-    # && sudo ln -s -f "$(command -v pip3)" /usr/bin/pip \
-    # && pip install \
-    #     cmake-format==0.6.10
+# Install CMake
+ARG CMAKE_VERSION=3.18
+ARG CMAKE_BUILD=4
+ARG CMAKE_INSTALL_DIR=/root
+RUN cd $CMAKE_INSTALL_DIR \
+    && wget https://cmake.org/files/v$CMAKE_VERSION/cmake-$CMAKE_VERSION.$CMAKE_BUILD-Linux-x86_64.tar.gz  \
+    && tar xzf cmake-$CMAKE_VERSION.$CMAKE_BUILD-Linux-x86_64.tar.gz  \
+    && rm -rf cmake-$CMAKE_VERSION.$CMAKE_BUILD-Linux-x86_64.tar.gz  \
+    && cd cmake-$CMAKE_VERSION.$CMAKE_BUILD-Linux-x86_64  \
+    && ./bin/cmake --version
 
+ENV PATH="${CMAKE_INSTALL_DIR}/cmake-${CMAKE_VERSION}.${CMAKE_BUILD}-Linux-x86_64/bin:${PATH}"
+
+# Install cmake-format
+RUN sudo pip install cmake-format==0.6.10
