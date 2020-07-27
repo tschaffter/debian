2ddb5daee46c4432e77f55a487d86a9cd5efc6c8
Thomas Schaffter <thomas.schaffter@gmail.com>
2020-07-26 20:05:33 -0700

    Add cxx dev environment (#5)
    * Add empty Dockerfile.esp
    
    * Remove apt lists in node env
    
    * Add cleanup steps to base environment
    
    * Add cleanup steps to all environments
    
    * Rename python env to conda; create new python env without conda
    
    * Separate install and version print into two RUN
    
    * Add cmake-format and clang-format to cxx env
    
    * Update to debian:buster-20200720-slim
    
    * Specify which files to hadolint in ci.yml
    
    * Update user environment
    
    * Update python and cxx envs
    
    * Update conda and synapse envs
    
    * Update docker env

---
+++ Dockerfile.cxx
@@ -0,0 +1,39 @@
+ARG version
+FROM tschaffter/debian:python-${version}
+
+ARG clang_format_version="11"
+
+# Install build tools
+RUN sudo apt-get update -qq -y \
+    && sudo apt-get install --no-install-recommends -qq -y \
+        cmake \
+        g++ \
+        gcc \
+        lcov \
+        ninja-build \
+    && sudo apt-get -y autoclean \
+    && sudo apt-get -y autoremove \
+    && sudo rm -rf /var/lib/apt-get/lists/*
+
+# Install cmake-format
+RUN sudo pip install cmake-format==0.6.10
+
+# Install clang-format (buster's release is several versions behind)
+RUN sudo apt-get update -qq -y \
+    && sudo apt-get install --no-install-recommends -qq -y \
+        gnupg2 \
+        software-properties-common \
+    && curl -fsSL https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add - \
+    && sudo add-apt-repository \
+        "deb [arch=amd64] https://apt.llvm.org/$(lsb_release -cs) \
+        llvm-toolchain-buster-11 \
+        main" \
+    && sudo apt-get update -qq -y \
+    && sudo apt-get install --no-install-recommends -qq -y \
+        clang-format-${clang_format_version} \
+    && sudo update-alternatives --install \
+        /usr/bin/clang-format clang-format \
+        /usr/bin/clang-format-${clang_format_version} 10 \
+    && sudo apt-get -y autoclean \
+    && sudo apt-get -y autoremove \
+    && sudo rm -rf /var/lib/apt-get/lists/*
